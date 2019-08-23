0757755b2c22007315418dff8a0e04d1da118110
mgmarlow <mgmarlow@gmail.com>
2019-08-23 09:46:24 -0700

    Initialize project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Open API Flow Type Generator
+
+Generate Flow types from the Open API spec.
---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import { builtinModules } from 'module';
+import pkg from './package.json';
+
+export default [
+  /* index.js */
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'index.js',
+      format: 'cjs',
+      sourcemap: true
+    },
+    external: Object.keys(pkg.dependencies).concat(builtinModules)
+  },
+
+  /* bin.js */
+  {
+    input: 'src/bin.js',
+    output: {
+      file: 'bin.js',
+      format: 'cjs',
+      banner: '#!/usr/bin/env node',
+      paths: {
+        flowgen: './index.js'
+      },
+      sourcemap: true
+    },
+    external: Object.keys(pkg.dependencies).concat(builtinModules, ['flowgen'])
+  }
+];
